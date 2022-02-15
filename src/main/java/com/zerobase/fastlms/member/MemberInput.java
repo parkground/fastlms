ff638397225ca41ce1f5b067debf1be1e643b42a
lomo <81scramble@gmail.com>
2022-02-15 21:19:48 +0900

    6-8 update

---
+++ src/main/java/com/zerobase/fastlms/member/MemberInput.java
@@ -0,0 +1,14 @@
+package com.zerobase.fastlms.member;
+
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+public class MemberInput {
+
+    private String userId;
+    private String userName;
+    private String password;
+    private String phone;
+}
