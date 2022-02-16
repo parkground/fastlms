9041d680496dfcfc62db82acdaf1ba7c5be64c82
lomo <81scramble@gmail.com>
2022-02-16 12:00:29 +0900

    6-10 update

--- src/main/java/com/zerobase/fastlms/MainController.java
+++ src/main/java/com/zerobase/fastlms/MainController.java
@@ -1,13 +1,24 @@
 package com.zerobase.fastlms;
 
+import com.zerobase.fastlms.components.MailComponents;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class MainController {
 
+    private final MailComponents mailComponents;
+
     @RequestMapping("/")
     public String index() {
+
+        String email = "81scramble@gmail.com";
+        String subject = " 안녕하세요. 제로베이스 입니다. ";
+        String text = "<p>HTML TEXT</p><p>테스트</p>";
+
+        mailComponents.sendMail(email, subject, text);
 
         return "index";
     }
