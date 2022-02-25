ec8a6c45466abed253e9014450bad14049bcc426
lomo <81scramble@gmail.com>
2022-02-25 13:02:14 +0900

    7-10 update

---
+++ src/main/java/com/zerobase/fastlms/course/entity/Course.java
@@ -0,0 +1,27 @@
+package com.zerobase.fastlms.course.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String imagePath;
+    String keyword;
+    String subject;
+
+    @Column(length = 1000)
+    String summary;
+
+    @Lob
+    String contents;
+    long price;
+    long salePrice;
+    LocalDateTime saleEndDt;
+}
